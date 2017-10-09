ef49759a694771ba14aca02052fce85a4137a2c6
Franke <joel.franke@thomsonreuters.com>
2017-10-09 15:17:16 -0400

    changes to individual scoring to show obst name based on hardcoded array

--- server/individual/individualScoring.js
+++ server/individual/individualScoring.js
@@ -1,13 +1,28 @@
 var aggJSON = [];
 var aggIndvJSON = [];
+
+var obstIndex = [{obstID: 1, value: "Water Carry"},
+    {obstID: 2, value: "Ninja Killer"},
+    {obstID: 3, value: "Hangman"},
+    {obstID: 4, value: "Half Dome"},
+    {obstID: 5, value: "Balancing Act"},
+    {obstID: 6, value: "Leap of Faith"},
+    {obstID: 7, value: "Slippery Wall Monkey"},
+    {obstID: 8, value: "Sclae the Walls"},
+    {obstID: 9, value: "Circus Maximus"},
+    {obstID: 10, value: "Skyclimb"},
+    {obstID: 11, value: "Rope Cross"},
+    {obstID: 12, value: "Over the Moon"}
+];
+
+
 function getResults(participantID,callback) {
   var jsonData = "https://blooming-ridge-76065.herokuapp.com/results/" + participantID;
   var personData = "https://blooming-ridge-76065.herokuapp.com/participant/" + participantID;
 
   $.getJSON( personData, function( personJson ) {
     var teamID = personJson.participant.teamID;
-    //currently the below code to evaluate teamID is not working
-    if (teamID !== null || teamID !== ""){
+    if (teamID !== null && teamID !== ""){
       var participantName = "<b>"+personJson.participant.lastName + ', ' + personJson.participant.firstName+"</b> ("+personJson.participant.teamID+")";
     } else {
       var participantName = "<b>"+personJson.participant.lastName + ', ' + personJson.participant.firstName+"</b>"
@@ -21,6 +36,19 @@ function getResults(participantID,callback) {
     for (var item in json.results) {
 
       var obstID = json.results[item].obstID;
+      //testing
+      var obstName;
+
+      Object.keys(obstIndex).forEach(function (key)
+        {
+          if(obstIndex[key].obstID == obstID){
+            obstName = obstIndex[key].value;
+            //need to pass obstName to the obstName callback
+          }
+        });
+      obstName = obstID + ". "+ obstName
+      // end testing
+
       var tier = json.results[item].tier;
       var deviceTime = json.results[item].deviceTime;
       var success = json.results[item].success;
@@ -31,7 +59,7 @@ function getResults(participantID,callback) {
         success = "&#x2715;"
       }
       var indivResultArray = [];
-      indivResultArray.push(obstID,tier,success,deviceTime);
+      indivResultArray.push(obstName,tier,success,deviceTime);
       aggIndvJSON.push(indivResultArray);
     }
   })
@@ -80,7 +108,7 @@ var participantID = getParameterByName('id');
 getResults(participantID,function(){
   setTimeout(function(){
     createPage(aggIndvJSON);
-}, 1400);
+}, 500);
 
 });
 //results reload every two minutes (120000ms)
